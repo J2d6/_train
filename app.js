003c7400583c93125cbd024488842cd96e2c03f3
j2d6 <j2d6.Pro@gmail.com>
2023-08-08 12:44:52 +0300

    add voayge and get voyagepredate

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var clientRouter = require("./routes/client");
 var categorieController = require("./routes/categorie");
 var wagonController = require("./routes/wagon");
-var trainRouter = require("./routes/train")
+var trainRouter = require("./routes/train");
+var voyageRouter = require("./routes/voyage");
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -23,4 +27,5 @@ app.use('/api/client', clientRouter);
 app.use('/api/categorie', categorieController);
 app.use('/api/wagons', wagonController);
 app.use("/api/train", trainRouter)
+app.use("/api/voyage", voyageRouter)
 module.exports = app;
